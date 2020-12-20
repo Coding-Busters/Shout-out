14ce857f2df11a564c09f938142cd6ce7b54f3fa
Puneet Kushwah <hacker7xy@gmail.com>
2020-12-20 23:19:16 +0530

    Design Sign up screen design and add a validation for unauthorized user.

--- app/src/main/java/com/shoutout/android/MainActivity.java
+++ app/src/main/java/com/shoutout/android/MainActivity.java
@@ -5,13 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SendUserToLoginActivity();
+
+        mAuth =FirebaseAuth.getInstance();
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser CurrentUser = mAuth.getCurrentUser();
+        if (CurrentUser == null){
+            SendUserToLoginActivity();
+        }else {
+
+        }
+
     }
 
     private void SendUserToLoginActivity(){
--- app/src/main/java/com/shoutout/android/SignUpActivity.java
+++ app/src/main/java/com/shoutout/android/SignUpActivity.java
@@ -4,53 +4,53 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
-import android.text.Editable;
 import android.text.Html;
-import android.text.TextWatcher;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 import com.google.android.material.textfield.TextInputLayout;
 
-public class LoginActivity extends AppCompatActivity {
-    private TextView Signup_text;
+public class SignUpActivity extends AppCompatActivity {
+
+    private TextView Login_text;
+    private TextInputLayout Email_Edit_Text,Password_Edit_Text,Full_Name_Edit_Text;
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_sign_up);
 
         setEditTexts();
-        Signup_text = findViewById(R.id.login_text_sign_up);
+        Login_text = findViewById(R.id.Sign_up_text_log_in);
 
-        String str = getColoredSpanned("I'm a new user.", "#272526");
-        String sign_up_text = getColoredSpanned("Sign Up","#FF4D83");
-        Signup_text.setText(Html.fromHtml(str+sign_up_text));
-        Signup_text.setOnClickListener(new View.OnClickListener() {
+        String str = getColoredSpanned("I'm already a member,", "#272526");
+        String sign_up_text = getColoredSpanned("Sign In","#FF4D83");
+        Login_text.setText(Html.fromHtml(str+sign_up_text));
+        Login_text.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SendUserToSignUpActivity();
+                SendUserToLoginActivity();
             }
         });
     }
 
-    private void SendUserToSignUpActivity() {
-        Intent SignUp_intent = new Intent(LoginActivity.this,SignUpActivity.class);
+    private void SendUserToLoginActivity() {
+        Intent SignUp_intent = new Intent(SignUpActivity.this,LoginActivity.class);
+        SignUp_intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(SignUp_intent);
     }
-
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     private void setEditTexts() {
 
         //Initialize both edit text
-        final TextInputLayout Email_Edit_Text = (TextInputLayout) findViewById(R.id.login_Email_Edit_text_input_layout);
-        final TextInputLayout Password_Edit_Text = (TextInputLayout) findViewById(R.id.login_password__edit_text_input_layout);
+        Email_Edit_Text = (TextInputLayout) findViewById(R.id.Sign_up_Email_Edit_text_input_layout);
+        Password_Edit_Text = (TextInputLayout) findViewById(R.id.Sign_up_password__edit_text_input_layout);
+        Full_Name_Edit_Text = (TextInputLayout) findViewById(R.id.Sign_up_FullName_Edit_text_input_layout);
+
 
         //Customize your Email and Password Edit text Here
         Email_Edit_Text.setBoxStrokeColor(getResources().getColor(R.color.soft_pink));
@@ -60,6 +60,10 @@ public class LoginActivity extends AppCompatActivity {
         Password_Edit_Text.setBoxStrokeColor(getResources().getColor(R.color.soft_pink));
         Password_Edit_Text.setHintTextColor(getColorStateList(R.color.soft_pink));
         Password_Edit_Text.setBoxCornerRadii(20,20,20,20);
+
+        Full_Name_Edit_Text.setBoxStrokeColor(getResources().getColor(R.color.soft_pink));
+        Full_Name_Edit_Text.setHintTextColor(getColorStateList(R.color.soft_pink));
+        Full_Name_Edit_Text.setBoxCornerRadii(20,20,20,20);
 
         //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     }
